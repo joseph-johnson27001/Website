09d8b0512b06a789cb74a6eff63039fb95e4aeaf
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2020-10-12 12:46:03 +0100

    Projects, animations and jumbotron
    Added logo on home page
    Added links for projects
    Added slide up animations for first three project areas
    Edited jumbotron area to be split into 2 (writing and image)

---
+++ js/home.app.js
@@ -0,0 +1,15 @@
+
+
+
+// jQuery function for icons and scrolling headers - Currently only for contact area
+      var heading = document.getElementById("contact-heading");
+  $(window).scroll(function() {
+      var top_of_element = $("#contact-form").offset().top;
+      var bottom_of_element = $("#contact-form").offset().top + $("#contact-form").outerHeight();
+      var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+      var top_of_screen = $(window).scrollTop();
+
+      if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+          heading.style.display = "block";
+      }
+  });
