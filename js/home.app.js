d3596df6bf15922d062fdaa0ba5ed14d3ce86239
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2020-10-16 11:03:29 +0100

    Test
    Test

--- js/home.app.js
+++ js/home.app.js
@@ -1,4 +1,10 @@
+// CONTACT AREA
+
 // jQuery function for icons and scrolling headers - Currently only for contact area
+
+
+// Below function will make the icon bounce and the contact heading scroll in from the left when the user scrolls to that area
+
       var heading = document.getElementById("contact-heading");
   $(window).scroll(function() {
       var top_of_element = $("#contact-form").offset().top;
@@ -12,44 +18,54 @@
   });
 
 
-// Function for sent/failed messages
+// Variables for contact form
 
 const messageButton = document.getElementById("message-button");
+const sentNotificationButton = document.getElementById("sent-button");
+const failedNotificationButton = document.getElementById("failed-button");
 const sentMessage = document.getElementById("sent-message");
 const failedMessage = document.getElementById("failed-message");
-
-messageButton.addEventListener("click", () => {
-  sentMessage.style.display = "none";
-  failedMessage.style.display = "none";
-    if (firstNameMessage.value == "" || secondNameMessage.value == "" || emailMessage.value == "" || messageMessage.value == "") {
-      failedMessageSend();
-      return
-    }
-    else {
-      sendMessage();
-      return
-    }
-});
-
-function sendMessage() {
-  sentMessage.style.display = "block";
-}
-
-function failedMessageSend() {
-  failedMessage.style.display = "block";
-}
-
-// TO DO FOR CONTACT FORM
-
-//CONTACT FORM VARIABLES
 
 let firstNameMessage = document.getElementById("first-name-message");
 let secondNameMessage = document.getElementById("second-name-message");
 let emailMessage = document.getElementById("email-message");
 let messageMessage = document.getElementById("message-message");
 
-// Give all form areas an id and make into variables
-// Check if they are blank
-// If any are blank run  failedMessageSend
-//If email is not an email = same thing
-//If all are correct = sendMessage()
+
+// Event listener for contact form send button
+
+messageButton.addEventListener("click", () => {
+    sentMessage.style.display = "none";
+    failedMessage.style.display = "none";
+      if (firstNameMessage.value == "" || secondNameMessage.value == "" || emailMessage.value == "" || messageMessage.value == "") {
+        failedMessageSend();
+      }
+      else {
+        sendMessage();
+      }
+});
+
+// Event listener to close sent message notification
+
+sentNotificationButton.addEventListener("click", () =>  {
+  sentMessage.style.display = "none";
+});
+
+
+// Event listener to close failed message notificaiton
+
+failedNotificationButton.addEventListener("click", () =>  {
+  failedMessage.style.display = "none";
+});
+
+// Send message functiom
+
+function sendMessage() {
+  sentMessage.style.display = "block";
+}
+
+// Failed message function
+
+function failedMessageSend() {
+  failedMessage.style.display = "block";
+}
